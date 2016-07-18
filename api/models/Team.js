276301cb7615a8afd271417a9071f1ddc3fcaac6
Bryan Gill <bryan@shipitweekly.com>
2016-07-17 20:50:51 -0500

    no message

---
+++ api/models/Team.js
@@ -0,0 +1,25 @@
+/**
+* Team.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    location: {
+      type: 'string'
+    },
+
+    maskot: {
+      type: 'string'
+    },
+
+    contract: {
+      model: 'league'
+    }
+  }
+};
+
