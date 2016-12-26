57cb371ecdbbc84a3669b10276d62ebd7f32d18f
Bryan Gill <bryan@shipitweekly.com>
2016-12-26 00:29:15 -0600

    Continuing to refactor statistics for power rankings

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -31,6 +31,7 @@ var init = function (req) {
   return params;
 
 };
+
 var configureQuery = function (year) {
   "use strict";
   var query = '';
@@ -52,106 +53,7 @@ var configureQuery = function (year) {
 var calculateStatistics = function (callback, games, count, year) {
   "use strict";
 
-  // return new Promise(function (resolve, reject) {
-
-  /* calculation variables */
-  // todo - implement statistics
-
   return Team.getTeams().populate('statistics');
-  // async.each(teams, function (team, callback) {
-  //
-  //   var pyth = 0;
-  //   var runsScored = 0;
-  //   var runsAllowed = 0;
-  //   var wins = 0;
-  //   var loses = 0;
-  //   var teamGames = 0;
-  //   var streak = 0;
-  //
-  //   /* annotate the teams */
-  //
-  //   async.each(games, function (game, callback) {
-  //     if ((wins + loses) < count) {
-  //       if (game.winning_team && (S(game.winning_team).contains(team.full_name))) {
-  //         wins = wins + 1;
-  //         teamGames = teamGames + 1;
-  //         runsScored = runsScored + game.winning_score;
-  //         streak = streak + 1;
-  //       } else if (game.losing_team && (S(game.losing_team).contains(team.full_name))) {
-  //         loses = loses + 1;
-  //         teamGames = teamGames + 1;
-  //         runsAllowed = runsAllowed + game.losing_score;
-  //         streak = streak - 1;
-  //
-  //       }
-  //       pyth = (runsScored * 1.81) / ((runsScored * 1.81) + (runsAllowed * 1.81));
-  //
-  //     }
-  //     callback();
-  //   }, function (err) {
-  //
-  //     var statistics = {
-  //       loses: 0,
-  //       wins: 0,
-  //       games_played: 0,
-  //       last20Wins: 0,
-  //       last20Losses: 0,
-  //       streak: 0,
-  //       rspg: 0,
-  //       rapg: 0,
-  //       sod: 0,
-  //       pyth: 0,
-  //       rating: 0
-  //     }
-  //
-  //     statistics.wins = wins;
-  //     statistics.loses = loses;
-  //     statistics.rspg = Math.round((runsScored / teamGames) * 100) / 100;
-  //     statistics.rapg = Math.round((runsAllowed / teamGames) * 100) / 100;
-  //     statistics.pyth = Math.round(pyth * 100) / 100;
-  //     statistics.games_played = teamGames;
-  //     statistics.streak = streak;
-  //
-  //     async.waterfall([
-  //       (callback) => {
-  //         "use strict";
-  //         teamService.getTeamRecord(team, year, 20)
-  //           .then(function (result) {
-  //             callback(null, games, team, result);
-  //           })
-  //       },
-  //       (games, team, result, callback) => {
-  //         "use strict";
-  //         teamService.sod(team, result, 162, games)
-  //           .then(function (result) {
-  //             callback(null, result);
-  //           })
-  //       },
-  //     ], function (err, result) {
-  //       statistics.last20Wins = result.record.wins;
-  //       statistics.last20Losses = result.record.loses;
-  //       statistics.sod = result.sod;
-  //
-  //       team.statistics = statistics;
-  //       team.statistics.rating = Math.round(((statistics.wins + statistics.last20Wins + (statistics.rspg / 2))
-  //           - (statistics.loses + statistics.last20Losses + (statistics.rapg / 2))
-  //           + (statistics.streak)
-  //           + (((statistics.sod - .5) * 100) / statistics.games_played)
-  //           + (statistics.pyth - .5)) * 100) / 100;
-  //
-  //       callback();
-  //     });
-  //   })
-  //
-  // }, function (err) {
-  //   var list = _.sortBy(teams, function (team) {
-  //     return team.statistics.rating;
-  //   })
-  //   list.reverse();
-  //   resolve(list);
-  // })
-  //     })
-  // })
 }
 
 var records = function (count, year) {
@@ -162,47 +64,29 @@ var records = function (count, year) {
   })
 }
 
+var statistics = function (count, year) {
+  "use strict";
+  return Team.getTeams().populate('statistics', {
+    limit: count,
+    sort: 'updatedAt DESC'
+  })
+}
+
 module.exports = {
 
   getStats: function (req, res) {
 
     var params = init(req);
-    var query = configureQuery(params.year);
 
-    async.waterfall([
-      (callback) => {
-        "use strict";
-        teamService.sortDate(query, 'desc')
-          .then(function (games) {
-            callback(null, games);
-          })
-      },
-      (games, callback) => {
-        calculateStatistics(callback, games, params.count, params.year)
-          .then(function (results) {
-            "use strict";
-            callback(null, results);
-          })
-      },
-    ], function (err, result) {
-      return res.send(result);
-    });
-
-  },
-
-  getRecords: function (req, res) {
-    "use strict";
-
-    var params = init(req);
-    // var query = configureQuery(params.year);
-
-    records(params.count, params.year)
+    statistics(params.count, params.year)
       .then(function (results) {
         "use strict";
         res.send(results);
       })
 
-
+    // var params = init(req);
+    // var query = configureQuery(params.year);
+    //
     // async.waterfall([
     //   (callback) => {
     //     "use strict";
@@ -212,7 +96,7 @@ module.exports = {
     //       })
     //   },
     //   (games, callback) => {
-    //     records(callback, games, params.count, params.year)
+    //     calculateStatistics(callback, games, params.count, params.year)
     //       .then(function (results) {
     //         "use strict";
     //         callback(null, results);
@@ -224,17 +108,17 @@ module.exports = {
 
   },
 
-  // getTeamRecord: function (req, res) {
-  //   "use strict";
-  //
-  //   var params = init(req);
-  //
-  //   teamService.getTeamRecord(params.team, params.year, params.count)
-  //     .then(function (result) {
-  //       return res.send(result);
-  //     })
-  // }
+  getRecords: function (req, res) {
+    "use strict";
+
+    var params = init(req);
+
+    records(params.count, params.year)
+      .then(function (results) {
+        "use strict";
+        res.send(results);
+      })
+
+  }
+
 }
-
-
-/* this method calculates the average pythagorean winning % of the other teams in your division */
--- api/models/Statistic.js
+++ api/models/Statistic.js
@@ -16,39 +16,156 @@ module.exports = {
   attributes: {
 
     file_id: {
-      type: 'string',
-      unique: true,
-      primaryKey: true
+      type: 'string'
     },
 
     team: {
       model: 'team'
+    },
+
+    wins: {
+      type: 'integer'
+    },
+
+    losses: {
+      type: 'integer'
+    },
+
+    games_played: {
+      type: 'integer'
+    },
+
+    last_20_wins: {
+      type: 'integer'
+    },
+
+    last_20_losses: {
+      type: 'integer'
+    },
+
+    streak: {
+      type: 'integer'
+    },
+
+    runs_scored_per_game: {
+      type: 'float'
+    },
+
+    runs_against_per_game: {
+      type: 'float'
+    },
+
+    strength_of_division: {
+      type: 'float'
+    },
+
+    pythagorian_w_pct: {
+      type: 'float'
+    },
+
+    rating: {
+      type: 'float'
     }
+
   },
 
-  map: function (a, res, model) {
-    "use strict";
-    var self = this;
 
-    var promise = new Promise(function (resolve, reject) {
+  /* logic to complete */
+  // async.each(teams, function (team, callback) {
+  //
+  //   var pyth = 0;
+  //   var runsScored = 0;
+  //   var runsAllowed = 0;
+  //   var wins = 0;
+  //   var loses = 0;
+  //   var teamGames = 0;
+  //   var streak = 0;
+  //
+  //   /* annotate the teams */
+  //
+  //   async.each(games, function (game, callback) {
+  //     if ((wins + loses) < count) {
+  //       if (game.winning_team && (S(game.winning_team).contains(team.full_name))) {
+  //         wins = wins + 1;
+  //         teamGames = teamGames + 1;
+  //         runsScored = runsScored + game.winning_score;
+  //         streak = streak + 1;
+  //       } else if (game.losing_team && (S(game.losing_team).contains(team.full_name))) {
+  //         loses = loses + 1;
+  //         teamGames = teamGames + 1;
+  //         runsAllowed = runsAllowed + game.losing_score;
+  //         streak = streak - 1;
+  //
+  //       }
+  //       pyth = (runsScored * 1.81) / ((runsScored * 1.81) + (runsAllowed * 1.81));
+  //
+  //     }
+  //     callback();
+  //   }, function (err) {
+  //
+  //     var statistics = {
+  //       loses: 0,
+  //       wins: 0,
+  //       games_played: 0,
+  //       last20Wins: 0,
+  //       last20Losses: 0,
+  //       streak: 0,
+  //       rspg: 0,
+  //       rapg: 0,
+  //       sod: 0,
+  //       pyth: 0,
+  //       rating: 0
+  //     }
+  //
+  //     statistics.wins = wins;
+  //     statistics.loses = loses;
+  //     statistics.rspg = Math.round((runsScored / teamGames) * 100) / 100;
+  //     statistics.rapg = Math.round((runsAllowed / teamGames) * 100) / 100;
+  //     statistics.pyth = Math.round(pyth * 100) / 100;
+  //     statistics.games_played = teamGames;
+  //     statistics.streak = streak;
+  //
+  //     async.waterfall([
+  //       (callback) => {
+  //         "use strict";
+  //         teamService.getTeamRecord(team, year, 20)
+  //           .then(function (result) {
+  //             callback(null, games, team, result);
+  //           })
+  //       },
+  //       (games, team, result, callback) => {
+  //         "use strict";
+  //         teamService.sod(team, result, 162, games)
+  //           .then(function (result) {
+  //             callback(null, result);
+  //           })
+  //       },
+  //     ], function (err, result) {
+  //       statistics.last20Wins = result.record.wins;
+  //       statistics.last20Losses = result.record.loses;
+  //       statistics.sod = result.sod;
+  //
+  //       team.statistics = statistics;
+  //       team.statistics.rating = Math.round(((statistics.wins + statistics.last20Wins + (statistics.rspg / 2))
+  //           - (statistics.loses + statistics.last20Losses + (statistics.rapg / 2))
+  //           + (statistics.streak)
+  //           + (((statistics.sod - .5) * 100) / statistics.games_played)
+  //           + (statistics.pyth - .5)) * 100) / 100;
+  //
+  //       callback();
+  //     });
+  //   })
+  //
+  // }, function (err) {
+  //   var list = _.sortBy(teams, function (team) {
+  //     return team.statistics.rating;
+  //   })
+  //   list.reverse();
+  //   resolve(list);
+  // })
+  //     })
+  // })
 
-      var obj = {
-        file_id: model.filename,
-        team: 'TOR' // check this - needs to match short name of team
-      }
-
-      // obj.home_score = self.determineHomeScore(obj, model);
-      // obj.visiting_score = self.determineVisitingScore(obj, model);
-      // obj.winning_team = self.determineWinner(obj);
-      // obj.losing_team = self.determineLoser(obj);
-      // obj.winning_score = self.determineWinningScore(obj);
-      // obj.losing_score = self.determineLosingScore(obj);
-
-      res.model = obj;
-      resolve(res);
-    });
-    return promise;
-  },
 
   /* todo move this to a service at some point */
   load: function (parser) {
@@ -58,18 +175,165 @@ module.exports = {
     return parser.getScores(parser.getDirectoryContentNames("input/recaps"));
   },
 
-  populate: function (callback, data) {
+  map: function (a, res, model) {
     "use strict";
-    Statistic.findOrCreate({ file_id: data.model.file_id }, data.model)
-      .then(function (data) {
-        "use strict";
-        callback();
-      })
-      .catch(function (error) {
-        sails.log.error(error.details);
-      });
+    var self = this;
+
+    var team_1 = {};
+    var team_2 = {};
+
+    return new Promise(function (resolve, reject) {
+
+      Game.find({game_id: model.filename})
+        .then(function (game) {
+
+          async.series([
+            function (callback) {
+              // var team = '';
+              TeamService.getShortName(game[0].winning_team)
+                .then(function (response) {
+                  team_1 = {
+                    file_id: model.filename,
+                    winning_team: response[0].short_name,
+                    winning_score: game[0].winning_score
+                  }
+                  callback();
+                })
+            },
+            function (callback) {
+              TeamService.getShortName(game[0].losing_team)
+                .then(function (response) {
+                  team_2 = {
+                    file_id: model.filename,
+                    losing_team: response[0].short_name,
+                    losing_score: game[0].losing_score
+                  }
+                  callback();
+                })
+            },
+            function (callback) {
+              var obj = [team_1, team_2]
+              res.model = obj;
+              resolve(res);
+              return callback();
+            }
+          ])
+        })
+    })
+  },
+
+  populate: function (callback, data, prefix_load) {
+    "use strict";
+
+    var self = this;
+
+    var team_1 = data.model[0];
+    var team_2 = data.model[1];
+
+    // todo prefix_load - preload this up with existing numbers
+
+    async.series([
+      function (callback) {
+
+        self.loadWLPrefix(prefix_load, team_1.winning_team + 'w');
+        self.loadRSRAPrefix(prefix_load, team_1.winning_team + 'rs', team_1.winning_score);
+        self.loadRSRAPrefix(prefix_load, team_1.winning_team + 'ra', team_2.losing_score);
+        var wins = prefix_load.get(team_1.winning_team + 'w');
+        var loses = prefix_load.get(team_1.winning_team + 'l');
+        // var pct = RecordService.calculateWinningPct(wins, loses);
+        var games_played = wins;
+        if (loses) {
+          if (games_played) {
+            games_played = games_played + loses;
+          } else {
+            games_played = loses;
+          }
+        }
+        var runs_scored_per_game = prefix_load.get(team_1.winning_team + 'rs') / games_played;
+        var runs_against_per_game = prefix_load.get(team_1.winning_team + 'ra') / games_played;
+
+        var obj = {
+          file_id: team_1.file_id,
+          team: team_1.winning_team,
+          wins: wins,
+          losses: loses,
+          games_played: games_played,
+          runs_scored_per_game: runs_scored_per_game,
+          runs_against_per_game: runs_against_per_game
+        }
+
+        Statistic.create(obj)
+          .then(function (data) {
+            "use strict";
+            callback();
+          })
+          .catch(function (error) {
+            sails.log.info(error.details);
+          });
+      },
+      function (callback) {
+
+        self.loadWLPrefix(prefix_load, team_2.losing_team + 'l');
+        self.loadRSRAPrefix(prefix_load, team_2.losing_team + 'rs', team_2.losing_score);
+        self.loadRSRAPrefix(prefix_load, team_2.losing_team + 'ra', team_1.winning_score);
+        var wins = prefix_load.get(team_2.losing_team + 'w');
+        var loses = prefix_load.get(team_2.losing_team + 'l');
+        var games_played = wins;
+        if (loses) {
+          if (games_played) {
+            games_played = games_played + loses;
+          } else {
+            games_played = loses;
+          }
+        }
+
+        var runs_scored_per_game = prefix_load.get(team_2.losing_team + 'rs') / games_played;
+        var runs_against_per_game = prefix_load.get(team_2.losing_team + 'ra') / games_played;
+
+        var obj = {
+          file_id: team_2.file_id,
+          team: team_2.losing_team,
+          wins: wins,
+          losses: loses,
+          games_played: games_played,
+          runs_scored_per_game: runs_scored_per_game,
+          runs_against_per_game: runs_against_per_game
+        }
+
+        Statistic.create(obj)
+          .then(function (data) {
+            "use strict";
+            callback();
+          })
+          .catch(function (error) {
+            sails.log.info(error.details);
+          });
+      },
+      function (callback) {
+        return callback();
+      }
+    ])
+  },
+
+  loadWLPrefix: function(prefix_load, team) {
+    "use strict";
+    if (prefix_load.has(team)) {
+      prefix_load.set(team, prefix_load.get(team) + 1)
+    } else {
+      prefix_load.set(team, 1)
+    }
+  },
+
+  loadRSRAPrefix: function(prefix_load, team, runs) {
+    "use strict";
+    if (prefix_load.has(team)) {
+      prefix_load.set(team, prefix_load.get(team) + runs)
+    } else {
+      prefix_load.set(team, runs)
+    }
 
   }
+
 
 };
 
